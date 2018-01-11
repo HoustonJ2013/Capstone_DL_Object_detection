3229b281216954cf1b850e6c03c430694136cde5
HoustonJ2013 <jerome198246@gmail.com>
2018-01-11 14:44:32 -0600

    test

--- models/pspnet_pred.py
+++ models/pspnet_pred.py
@@ -291,6 +291,7 @@ if __name__ == "__main__":
             class_scores = predict_multi_scale(img, pspnet, EVALUATION_SCALES, args.sliding, args.flip)
 
             print(pspnet.model.summary())
+            print(pspnet.get_weights())
             print("Predicting for ", input_name)
             class_image = np.argmax(class_scores, axis=2)
             output_name = input_name.split("/")[-1].replace(".jpg", "").replace(".png", "")
