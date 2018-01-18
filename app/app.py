ae2902ff02e297fd206c8fc3910096ee1eed54d2
HoustonJ2013 <jerome198246@gmail.com>
2018-01-18 15:56:47 -0600

    app

--- app/app.py
+++ app/app.py
@@ -14,15 +14,15 @@ with open('data/model.pkl') as f:
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Segmantic Segmentation', data=None)
+    return render_template('index.html', title='Understanding Your Image at Pixel Level', data=None)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
     doc = request.form['text1']
     model_select = request.form["model_selected"]
-    if model_select == "Default(resnet34_dilated8 + c1_bilinear)":
-        model_c = model
+    # if model_select == "Default(resnet34_dilated8 + c1_bilinear)":
+    #     model_c = model
     # elif model_select == "Model2":
     #     model_c = model_2
     # elif model_select == "Model3":
@@ -32,7 +32,7 @@ def submit():
         doc = [doc]
     sec_name = model_c.predict(doc)
     proba = [np.max(model_c.predict_proba(doc))]
-    return render_template('index.html', title='Named Entity Recognition', data=zip(doc,sec_name,proba))
+    return render_template('index.html', title='Understanding Your Image at Pixel Level', data=zip(doc,sec_name,proba))
 
 
 
