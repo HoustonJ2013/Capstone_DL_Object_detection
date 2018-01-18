0e53fedd784236ef19d432956370eb75f9d9671a
Ubuntu <ubuntu@ip-172-31-21-137.us-east-2.compute.internal>
2018-01-18 22:01:38 +0000

    app

--- app/app.py
+++ app/app.py
@@ -17,16 +17,6 @@ def index():
 def submit():
     doc = request.form['text1']
     model_select = request.form["model_selected"]
-<<<<<<< HEAD
-#    if model_select == "Default(resnet34_dilated8 + c1_bilinear)":
-#        model_c = model
-    if isinstance(doc, basestring):
-        doc = [doc]
-        #sec_name = model_c.predict(doc)
-        #proba = [np.max(model_c.predict_proba(doc))]
-        return render_template('index.html', title='Named Entity Recognition', data=0)
-                                                                    
-=======
     # if model_select == "Default(resnet34_dilated8 + c1_bilinear)":
     #     model_c = model
     # elif model_select == "Model2":
@@ -39,9 +29,6 @@ def submit():
     # sec_name = model_c.predict(doc)
     # proba = [np.max(model_c.predict_proba(doc))]
     return render_template('index.html', title='Understanding Your Image at Pixel Level', data=zip(doc,sec_name,proba))
-
->>>>>>> ae2902ff02e297fd206c8fc3910096ee1eed54d2
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
