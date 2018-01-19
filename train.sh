d0434945ffba60c678a9b10e3bebc43b268e7be5
Ubuntu <ubuntu@ip-172-31-7-58.us-east-2.compute.internal>
2018-01-19 04:57:10 +0000

    train test

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-python models/pspnet_train.py -train data/data_train.txt -lab data/data_label.txt --weights pspnet50_ade20k --num_epoch 2 --learning_rate 0.0001 >  train_log.txt 
+python models/pspnet_train.py -train data/data_train.txt -lab data/data_label.txt --weights pspnet50_ade20k --num_epoch 1 --learning_rate 0.00001 >  train_log.txt 
 
-#aws ec2 stop-instances --instance-ids i-0c7ec9b8c34e4912a 
+aws ec2 stop-instances --instance-ids i-0c7ec9b8c34e4912a 
