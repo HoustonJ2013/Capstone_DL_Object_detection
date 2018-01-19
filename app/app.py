d3563a73bc83a6cbfe3c7b6c88b6dce6388439b8
HoustonJ2013 <jerome198246@gmail.com>
2018-01-19 16:49:07 -0600

    app

--- app/app.py
+++ app/app.py
@@ -37,7 +37,7 @@ def run():
         Capnet = PSPNet50(nb_classes=150, input_shape=(473, 473),
                           weights="pspnet50_ade20k")
         print("     AF Init Model", str(datetime.now()), datetime.now() - TIME_START)
-        Capnet.predict(input_list, flip, output_path="/static/", batch_size=5)
+        Capnet.predict(input_list, flip, output_path="static/", batch_size=5)
 
     pic_pred = ["/static/validation_ADE_val_00000661.png", "/static/validation_ADE_val_00000661.png"]
     return render_template('index.html',  data=pic_pred)
