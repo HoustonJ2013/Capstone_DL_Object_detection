dd12f94dee86f1a400a940dd75bc0dd0810c2e09
Ubuntu <ubuntu@ip-172-31-21-137.us-east-2.compute.internal>
2018-01-19 16:52:53 +0000

    merge

--- predtest.sh
+++ predtest.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 python models/pspnet_pred.py -m pspnet50_ade20k --input_list psptest1_list -f --output_path  results/ --weights  pspade20k_epoch0_sgd_lrn5
-=======
-python models/pspnet_pred.py -m pspnet50_ade20k --input_list psptest1_list -f --output_path  results/ --weights pspade20k_epoch0_adam_lrn5
->>>>>>> 7c5f1b97e5452d9aa2c2ef76c0f854a8b0bf5941
 
 python src/metrics_acc_iou.py --List_predict pred_list --List_true label_list 
 
--- train.sh
+++ train.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-python models/pspnet_train.py -train data/data_train.txt -lab data/data_label.txt --weights pspnet50_ade20k --num_epoch 2 --learning_rate 0.00001 -optimizer SGD >  train_log_aws1.txt 
-=======
-python models/pspnet_train.py -train data/data_train.txt -lab data/data_label.txt --weights pspnet50_ade20k --num_epoch 1 --learning_rate 0.00001 >  train_log.txt 
->>>>>>> 7c5f1b97e5452d9aa2c2ef76c0f854a8b0bf5941
-
-aws ec2 stop-instances --instance-ids i-0c7ec9b8c34e4912a 
+python models/pspnet_train.py -train data/data_train.txt -lab data/data_label.txt --weights pspnet50_ade20k --num_epoch 2 --learning_rate 0.00001 --optimizer SGD >  train_log_aws1.txt 
+#aws ec2 stop-instances --instance-ids i-0c7ec9b8c34e4912a 
