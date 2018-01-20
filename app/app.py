43373d324595f69710ac341000ab76787d347f76
HoustonJ2013 <jerome198246@gmail.com>
2018-01-20 15:49:49 -0600

    app

--- app/app.py
+++ app/app.py
@@ -107,7 +107,7 @@ def run():
 
         pred_array = pred_array.flatten()
         pred_array = pred_array[pred_array > 0]
-        color_list = np.array([tem[0] for tem in Counter(pred_array).most_common(10)])
+        color_list = np.array([tem[0] for tem in Counter(pred_array).most_common(6)])
         new_im = colorlabel(color_list)
         new_im.save("static/" + picture[:-4] + "_color.jpg")
         print("finished color")
